624206f0382afce4d7ce3e036881ea8a031d7e34
Adam Ochonicki <fromonesrc@users.noreply.github.com>
2017-10-10 21:21:03 -0400

    Say what's playing

---
+++ README.md
@@ -0,0 +1,40 @@
+# slacktunes
+
+Set your Slack status to the current iTunes track
+
+## How?
+
+Get a Slack token (or tokens if you want to do this across multiple Slack workspaces) or use your [test Slack token](https://api.slack.com/docs/oauth-test-tokens).
+
+Get your Slack User ID (or IDs if you want to do this across multiple Slack workspaces):
+
+`curl -s 'https://slack.com/api/auth.test?token=your_token_here' | jq .user_id`
+
+Create an `.env` file and replace with your own Slack token(s) and user ID(s). For example:
+
+```
+SLACK_TOKENS=xoxp-xxxxxxxxxx-xxxxxxxxxx-xxxxxxxxxxx-xxxxxxxxxx,xoxp-xxxxxxxxxx-xxxxxxxxxx-xxxxxxxxxxx-xxxxxxxxxx
+SLACK_USERS=U12345678,U23456789
+```
+
+Run `ruby slacktunes.rb`
+
+## Scheduling
+
+Run it on a cron.
+
+Add this to your `~/.bashrc` so cron doesn't 😧 about any special ❄️ characters
+
+```
+export LANG=en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+```
+
+Add this to your crontab with `crontab -e` to run it every two minutes:
+
+```
+SHELL=/bin/bash
+BASH_ENV="~/.bashrc"
+*/2 * * * * cd /path/to/slacktunes && /path/to/bundle exec ruby slacktunes.rb > /dev/null
+```
---
+++ slacktunes.rb
@@ -0,0 +1,50 @@
+#!/usr/bin/env ruby
+
+require 'dotenv'
+require 'json'
+require 'open-uri'
+
+Dotenv.load
+
+def get_track
+  %x{ osascript <<APPLESCRIPT
+    tell application "iTunes"
+      set track_artist to the artist of the current track
+      set track_name to the name of the current track
+      set track_display to "Listening to " & track_artist & ": " & track_name
+    end tell
+    return track_display
+  APPLESCRIPT }
+end
+
+status_emoji = URI::encode(":headphones:")
+
+tokens = ENV['SLACK_TOKENS'].split(',')
+users = ENV['SLACK_USERS'].split(',')
+
+token_users = tokens.zip(users)
+
+token_users.each do |token, user|
+  presence_response = `curl -s 'https://slack.com/api/users.getPresence?token=#{token}&user=#{user}'`
+  presence = JSON.parse(presence_response)['presence']
+
+  profile = `curl -s 'https://slack.com/api/users.profile.get?token=#{token}'`
+  status_text = JSON.parse(profile)['profile']['status_text'] || ''
+
+  if presence == 'active' && status_text && (status_text.include?("Listening to") || status_text.empty?)
+    track = URI::encode(get_track)
+    track.gsub!("'",'%27')
+    track.gsub!('?','%3F')
+    track.gsub!('+','%2B')
+    track.gsub!('&','%26')
+    track.gsub!('[','%5B')
+    track.gsub!(']','%5D')
+    track.gsub!('(','%5B')
+    track.gsub!(')','%5D')
+    track.gsub!('%0A','')
+    track = track[0..99]
+    curl = "curl -s -XPOST 'https://slack.com/api/users.profile.set?token=#{token}&profile=%7B%22status_emoji%22%3A%22#{status_emoji}%22%2C%22status_text%22%3A%22#{track}%22%7D'"
+    `#{curl}`
+  end
+end
+
